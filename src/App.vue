264e4ab4357ed2a2ae40eefe09328d110244ed1c
Cytan2000 <tanchiyong00@gmail.com>
2022-10-09 01:01:40 +0800

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <div>
+  <Navbar></Navbar>
   <router-view></router-view>
+  </div>
 </template>
 <script>
+import Navbar from "./components/Navbar.vue";
 export default {
-  name: 'App',
+    name: "App",
+    components: { Navbar }
 }
 </script>
 
