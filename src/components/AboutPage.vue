995be4be92f9fc42e6c042a5af5af479a7cf8a1b
Cytan2000 <tanchiyong00@gmail.com>
2022-10-09 00:36:11 +0800

    routing and css

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <p class="font-sans">About</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import AboutPage from './components/AboutPage'
+import HomePage from './components/HomePage'
+import NotFound from './components/NotFound'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        { path: '/about', component: AboutPage},
+        { path: '/', component: HomePage},
+        { path: "/:pathMatch(.*)*", component: NotFound }
+     ]
+});
+
+const app= createApp(App)
+
+app.use(router)
+
+app.mount('#app')
