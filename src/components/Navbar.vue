f5d6f18cc3048b795bdacb8212be9f2ae9dddc13
Cytan2000 <tanchiyong00@gmail.com>
2022-10-13 16:25:12 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <Disclosure as="nav" class="bg-yellow-900" v-slot="{ open }">
+    <Disclosure as="nav" class="" v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-20">
         <div class="relative flex h-20 items-center justify-between">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hiddene">
             <!-- Mobile menu button-->
             <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span class="sr-only">Open main menu</span>
