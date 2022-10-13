71520a4773f06ef4990a4551194a2fe6f4d444ab
Joel Tan <joeltanec@gmail.com>
2022-10-13 22:03:38 +0800

    [Not Working] - Starter Code for Login page...
    Need to go back and rectify the code for login page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -9,7 +9,7 @@
 <script setup>
 import { ref } from "vue";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 const email = ref("");
 const password = ref("");
 const router = useRouter()
@@ -20,7 +20,7 @@ const register = () =>{
     createUserWithEmailAndPassword(getAuth(),email.value,password.value)
     .then((data)=>{
         console.log("Successfully registered!");
-        router.push("/HomePage")
+        router.push("/")
     })
     .catch((error)=>{
         console.log(error.code);
@@ -29,7 +29,7 @@ const register = () =>{
 };
 
 const signInWithGoogle = () =>{
-    
+
 }
 
 
