a949954e22477134c8a46a09c598981be34f1b53
Cytan2000 <tanchiyong00@gmail.com>
2022-10-18 10:29:57 +0800

    hamburger

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
     <Disclosure as="nav" class="sticky top-0 backdrop-filter backdrop-blur-lg" v-slot="{ open }">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 h-20">
         <div class="relative flex h-20 items-center justify-between">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hiddene">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <!-- Mobile menu button-->
             <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span class="sr-only">Open main menu</span>
