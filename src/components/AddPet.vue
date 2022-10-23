62d9953a2766b0da486a5bc406796e5cacad4733
Cytan2000 <tanchiyong00@gmail.com>
2022-10-23 21:44:04 +0800

    Update AddPet.vue

--- src/components/AddPet.vue
+++ src/components/AddPet.vue
@@ -19,10 +19,17 @@
             placeholder="Pet Age"
             v-model="page"
           />
-    
+          <button @click="click1" >choose a photo</button>
+           <input type="file" ref="input1"
+            style="display: none"
+            @change="previewImage" accept="image/*" >  
+                          
+          <img v-if="imageData!=null" class="preview" height="268" width="356" :src="img1">
     <button 
             @click="submit_pet_post"
+            
             type="submit"
+            id="submit_link"
             class="py-4 bg-blue-400 w-full rounded text-blue-50 font-bold hover:bg-blue-700 my-2">
             Submit
           </button>
@@ -30,10 +37,8 @@
 
 <script>
 import { getAuth }  from "firebase/auth";
-import { getDatabase, ref, set,update } from "firebase/database";
-
-
-
+import { getStorage,getDatabase, ref, set,update } from "firebase/database";
+import 'firebase/storage';
 
 function writeUserData(userId,pname,pbreed,page) {
   const db = getDatabase();
@@ -51,6 +56,7 @@ export default {
       pname:"",
       pbreed:"",
       page:"",
+      selectedFile:"",
     }
   },
   methods:{
@@ -59,9 +65,49 @@ export default {
         const user = auth.currentUser;
         console.log(this.pname,this.pbreed,this.page);
         writeUserData(user.uid,this.pname,this.pbreed,this.page);
-  }
+        this.create();
+
+  },
+  previewImage(event) {
+  this.uploadValue=0;
+  this.img1=null;
+  this.imageData = event.target.files[0];
+  this.onUpload()
+},
+click1() {
+  this.$refs.input1.click()   
+},
+  onUpload(){
+  this.img1=null;
+  const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+  storageRef.on(`state_changed`,snapshot=>{
+  this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+    }, error=>{console.log(error.message)},
+  ()=>{this.uploadValue=100;
+      storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+          this.img1 =url;
+          console.log(this.img1)
+        });
+      }      
+    );
+ },create () {
+      
+      const post = {
+        photo: this.img1,
+        caption: this.caption        
+      }
+
+      firebase.database().ref('PhotoGallery').push(post)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  
 }
-}
+}			
 </script>
 
 <style>
