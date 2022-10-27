f841137038c47876f35deb1893e901ab1b7a4e04
Cytan2000 <tanchiyong00@gmail.com>
2022-10-27 22:54:27 +0800

    create card for buyer

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,32 +6,36 @@
     
   
   
-    <div class="flex flex-col md:flex-row items-stretch justify-between dark:bg-gray-800  md:py-12 lg:px-12 ">
-      <div class="flex flex-col items-start md:w-1/2 ml-40 mt-40">
-        <h1 class="text-3xl lg:text-4xl font-semibold text-black px-3">Find a Sitter now!</h1>
-        <p class="text-base lg:text-xl text-black  mt-2 px-3">Save up to <span class="font-bold">50%</span></p>
-        <button class="bg-transparent hover:bg-yellow-500 text-black font-semibold hover:text-blue py-2 px-4 border border-yellow-500 hover:border-transparent rounded">
-  Click Here
-</button>
+    <div>
+        <buyer-card>
+          <template v-slot:description>
+              <h1>gamer</h1>
+          </template>
+        </buyer-card>
+        <buyer-card></buyer-card>
       </div>
-
-      <div class="md:w-max mt-8 md:mt-0 flex justify-center md:justify-end">
+      <div class="md:w-max  flex justify-center md:justify-end">
         <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-design" />
       </div>
    
     
   
-</div>
+
 <div class="my-20 text-black">
       Add more stuff IG
     </div>
 </template>
 
 <script>
-
+import BuyerCard from './UI/buyerCard.vue'
+export default {
+  components: { BuyerCard }
+}
 </script>
 
 
 
 
 
+
+BuyerCard
---
+++ src/components/UI/buyerCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <a href="#" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <slot name="seller">
+                <h4>Seller</h4>
+            </slot>
+        </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <slot name="description">
+                <p>Farmer</p>
+            </slot>
+            <slot name="rating"></slot>
+        </p>
+    </div>
+    </a>
+</template>
+
+<script>
+
+
+export default ({
+    
+})
+</script>
