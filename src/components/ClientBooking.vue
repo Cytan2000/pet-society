2996566c96d9e1064214dd50a87ce37da9833d55
Cytan2000 <tanchiyong00@gmail.com>
2022-11-06 01:30:15 +0800

    client booking page(in progress)

--- src/components/ClientBooking.vue
+++ src/components/ClientBooking.vue
@@ -1,3 +1,72 @@
+<style>
+@import "./ClientBooking.css";
+</style>
+
 <template>
-    
+  <div>
+    <div class="grid grid-cols-3 gap-4 h-full md:flex-row">
+      <div class="h-26 flex-auto col-span-2" >
+        <div
+          href="#"
+          class="flex flex-col items-center bg-white border md:flex-row md:max-w-full  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          <img
+            class="image-sizing mx-5 my-5"
+            src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              Pet Sitting for Small Pets Only(East Coast) @ Katong
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Reviews
+            </p>
+
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Statistics
+            </p>
+          </div>
+        </div>
+        <div class="border-t-2 border-b-2 py-5">
+            <!-- add location here -->
+            Location
+        </div>
+        <div>
+            About Pet Sitting For Small Pets Only (East Coast)
+Summary.
+
+My daughter (Shermaine) and I are providing pet sitting services for birds only. We are both animal lovers. She works at the zoo while I work in the turf club. <br/><br/>
+
+As a standard practice, food/cage/stand/toys have to be provided by the owner so that your pet(s) will have a similar diet and feel comfortable in a new environment. We will:
+- send video daily
+- replenish and clean up daily
+- ensure sufficient playtime
+- shower/bathe them at least once every 5 days.
+
+If you like to, our pet sitting services are customisable and on a request basis.
+
+Pick up / Drop off services are also available.
+
+
+
+Number of pets that will be watched at one time.
+
+6
+        </div>
+      </div>
+
+
+
+      <div class="bg-blue-500 text-white h-26 flex-auto">
+        <p>Flex item 2</p>
+      </div>
+
+
+
+    </div>
+  </div>
 </template>
+
+<script></script>
