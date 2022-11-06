ddf3807170282a06eb6094d6867e63678fc6f5a5
elimjw <114982470+elimjw@users.noreply.github.com>
2022-11-06 16:37:50 +0800

    moved google maps to homepage

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,16 @@
   <router-view></router-view>
   <div></div>
   </div>
-  <div class="d-flex text-center" style="height: 20vh">
-    <div class="m-auto">
-      <h4>Your Position</h4>
-      Latitude: {{ currPos.lat.toFixed(2) }}, 
-      Longitude: {{ currPos.lng.toFixed(2) }}
-    </div>
-  </div>
-  <div ref="mapDiv" style="width: 100%; height: 80vh" />
+  
 </template>
 <script>
 
 import NavBar from "./components/Navbar.vue";
-import { computed, ref, onMounted } from 'vue';
-import { useGeolocation } from "./useGeolocation";
-import { Loader } from '@googlemaps/js-api-loader';
 
-const GOOGLE_MAPS_API_KEY = 'AIzaSyCukyKCDxVUE4SitWeADOGFdaW6hWH9T20'
 
 export default {
   name: "App",
   components: { NavBar },
-  setup() {
-    const { coords } = useGeolocation()
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude
-    }))
-
-    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
-    const mapDiv = ref(null)
-    onMounted(async () => {
-      await loader.load()
-      new google.maps.Map(mapDiv.value, {
-        center: currPos.value,
-        zoom: 7
-      })
-    })
-    return { currPos, mapDiv }
-  }
+  
 }
 </script>
