b8a8f7b04b8fcb6c2c8d735ec2edd0584913403b
Cytan2000 <tanchiyong00@gmail.com>
2022-11-08 18:45:55 +0800

    homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,103 +1,102 @@
 <style>
-  @import './HomePage.css';
+@import "./HomePage.css";
 </style>
 
 <template>
-    
-  
-  
-    <div v-for="seller in list1">
-        <buyer-card>
-          <template v-slot:name>
-              <h1>{{seller.firstname}}</h1>
-          </template >
-          <template v-slot:description>
-            <p>{{seller.email}}</p>
-          </template>
-        </buyer-card>
-        
+  <section class="container mx-auto">
+    <div class="grid grid-cols-3 gap-4">
+      <div class="col-span-2">
+        <div v-for="seller in list1">
+          <buyer-card>
+            <template v-slot:name>
+              <h1>{{ seller.firstname }}</h1>
+            </template>
+            <template v-slot:description>
+              <p>{{ seller.email }}</p>
+            </template>
+          </buyer-card>
+        </div>
       </div>
-      <div class="md:w-max  flex justify-center md:justify-end">
-        <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-design" />
-      </div>
-   
-    
-  
+      <div class="col-span-1">
+        <div class="d-flex text-center" >
+    <div class="m-auto">
+      <!-- <h4>Your Position</h4>
+      Latitude: {{ currPos.lat.toFixed(2) }}, Longitude:
+      {{ currPos.lng.toFixed(2) }} -->
+    </div>
+  </div>
+  <div ref="mapDiv" style="width: 100%; height: 80vh" />
 
-    <div class="d-flex text-center" style="height: 20vh">
-      <div class="m-auto">
-        <h4>Your Position</h4>
-        Latitude: {{ currPos.lat.toFixed(2) }}, 
-        Longitude: {{ currPos.lng.toFixed(2) }}
       </div>
     </div>
-    <div ref="mapDiv" style="width: 100%; height: 80vh" />
+  </section>
+
+  <!-- <div class="md:w-max  flex justify-center md:justify-end">
+        <img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-design" />
+      </div> -->
+
+ 
 </template>
 
 <script>
-
-import BuyerCard from './UI/buyerCard.vue'
-import { computed, ref, onMounted } from 'vue';
+import BuyerCard from "./UI/buyerCard.vue";
+import { computed, ref, onMounted } from "vue";
 import { useGeolocation } from "./useGeolocation";
-import { Loader } from '@googlemaps/js-api-loader';
+import { Loader } from "@googlemaps/js-api-loader";
 import { getDatabase, ref as stoRef, onValue, child } from "firebase/database";
 
-
-
-const GOOGLE_MAPS_API_KEY = 'AIzaSyCukyKCDxVUE4SitWeADOGFdaW6hWH9T20'
+const GOOGLE_MAPS_API_KEY = "AIzaSyCukyKCDxVUE4SitWeADOGFdaW6hWH9T20";
 
 export default {
   components: { BuyerCard },
-  data(){
+  data() {
     return {
-      list1:[]
-    }
+      list1: [],
+    };
   },
   setup() {
-    const { coords } = useGeolocation()
+    const { coords } = useGeolocation();
     const currPos = computed(() => ({
       lat: coords.value.latitude,
-      lng: coords.value.longitude
-    }))
+      lng: coords.value.longitude,
+    }));
 
-    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
-    const mapDiv = ref(null)
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
+    const mapDiv = ref(null);
     onMounted(async () => {
-      await loader.load()
+      await loader.load();
       new google.maps.Map(mapDiv.value, {
         center: currPos.value,
-        zoom: 7
-      })
-    })
-    return { currPos, mapDiv }
+        zoom: 7,
+      });
+    });
+    return { currPos, mapDiv };
   },
-  methods:{
-    getSeller(){
+  methods: {
+    getSeller() {
       const db = getDatabase();
-      const dbRef = stoRef(db, 'users/');
+      const dbRef = stoRef(db, "users/");
 
-      onValue(dbRef, (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-          const childKey = childSnapshot.key;
-          const childData = childSnapshot.val();
-          if (childData["acc_type"]=="seller"){
-          this.list1.push(childData);
-          console.log(this.list1)
-          }
-  });
-}, {
-  onlyOnce: true
-});
-    }
+      onValue(
+        dbRef,
+        (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            const childKey = childSnapshot.key;
+            const childData = childSnapshot.val();
+            if (childData["acc_type"] == "seller") {
+              this.list1.push(childData);
+              console.log(this.list1);
+            }
+          });
+        },
+        {
+          onlyOnce: true,
+        }
+      );
+    },
   },
-  mounted(){
+  mounted() {
     this.getSeller();
-  }
-}
+  },
+};
 </script>
-
-
-
-
-
-
