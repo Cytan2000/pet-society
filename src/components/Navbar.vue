d9a40c31e04b97158968291878d84e30f31376a2
Cytan2000 <tanchiyong00@gmail.com>
2022-11-09 15:09:58 +0800

    update cards

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -51,13 +51,13 @@
               </div>
               <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                 <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <MenuItem v-slot="{ active }" v-if="!isBuyer">
+                  <MenuItem v-slot="{ active }" v-if="account=='seller'">
                     <a href="/seller/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Profile</a>
                   </MenuItem>
-                  <MenuItem v-slot="{ active }" v-if="isBuyer">
+                  <MenuItem v-slot="{ active }" v-if="account=='buyer'">
                     <a href="/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Profile</a>
                   </MenuItem>
-                  <MenuItem v-slot="{ active }">
+                  <MenuItem v-slot="{ active }" v-if="isLoggedIn">
                     <a href="/about" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Settings</a>
                   </MenuItem>
                   <MenuItem v-slot="{ active }" v-if="!isLoggedIn">
@@ -105,7 +105,7 @@ const auth = getAuth();
 const user = auth.currentUser;
 const isLoggedIn = ref(false);
 const base_data=JSON.parse(localStorage.getItem("db_data"));
-var account=ref("");
+var account=ref("")
 var isBuyer=ref("")
 
 
--- src/components/UI/buyerCard.vue
+++ src/components/UI/buyerCard.vue
@@ -1,18 +1,38 @@
 <template>
-    <a href="/client" class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <a href="/client" class="flex flex-row mb-5 items-center bg-white rounded-lg border shadow-md md:flex-row max-w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
     <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
     <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <div class="flex flex-row">
+            <div>
+                <img class="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/194/194630.png"/>
+            </div>
+            <div>
+                <img class="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/185/185846.png"/>
+            </div>
+            
+        </div>
+        
+        <h5 class="mb-2 tracking-tight text-gray-900 dark:text-white">
             <slot name="name">
-                <h4>Seller</h4>
+                <h2 class="font-bold text-xl ">Pet Sitting for Small pets Only</h2>
             </slot>
         </h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <slot name="description">
                 <p>Farmer</p>
             </slot>
-            <slot name="rating"></slot>
         </p>
+        <div>
+            <slot name="rating"></slot>
+        </div>
+        <div class="flex flex-row">
+            
+        </div>
+    </div>
+    <div class="ml-auto mb-auto">
+        From 
+        <p >SGD 30</p>
+        <p>/night</p>
     </div>
     </a>
 </template>
