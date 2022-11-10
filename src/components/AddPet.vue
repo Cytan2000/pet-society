4c55bc527dc18842d2a603c1a0e1e30b8063957d
Cytan2000 <tanchiyong00@gmail.com>
2022-11-10 14:59:35 +0800

    edit homepage

--- src/components/AddPet.vue
+++ src/components/AddPet.vue
@@ -1,131 +1,166 @@
 <template>
-  Pet Type: 
-        <select name="animal" id="animal_type" v-model="animal_type" class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500">
-          <option value="Dog">Dog</option>
-          <option value="Cat">Cat</option>
-          <option value="Kangaroo">Kangaroo</option>
-          <option value="Giraffe" selected>Giraffe</option>
-        </select>
-        Pet Name<input
-            type="text"
-            class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
-            placeholder="Pet name"
-            v-model="pname"
-          />
-    
-      Pet Breed<input
-            type="text"
-            class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
-            placeholder="Pet Breed"
-            v-model="pbreed"
-          />
-    
-      Pet Age<input
-            type="text"
-            class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
-            placeholder="Pet Age"
-            v-model="page"
-          />
-         
-      Upload Pet Image
-            <div class="flex">
+  Pet Type:
+  <select
+    name="animal"
+    id="animal_type"
+    v-model="animal_type"
+    class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
+  >
+    <option value="Dog">Dog</option>
+    <option value="Cat">Cat</option>
+    <option value="Kangaroo">Kangaroo</option>
+    <option value="Giraffe" selected>Giraffe</option>
+  </select>
+  Pet Name<input
+    type="text"
+    class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
+    placeholder="Pet name"
+    v-model="pname"
+  />
+
+  Pet Breed<input
+    type="text"
+    class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
+    placeholder="Pet Breed"
+    v-model="pbreed"
+  />
+
+  Pet Age<input
+    type="text"
+    class="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
+    placeholder="Pet Age"
+    v-model="page"
+  />
+
+  Upload Pet Image
+  <div class="flex">
     <div class="w-full rounded-lg">
-        <div class="">
-            <label class="inline-block mb-2 text-gray-500"></label>
-            <div class="flex items-center justify-center w-full">
-                <label
-                    class="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
-                    <div class="flex flex-col items-center justify-center pt-7">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400 group-hover:text-gray-600"
-                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                        </svg>
-                        <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-                            Attach a file</p>
-                    </div>
-                    <input type="file" ref="input1" class="opacity-0" @change="previewImage" accept="image/*"/>
-                </label>
+      <div class="">
+        <label class="inline-block mb-2 text-gray-500"></label>
+        <div class="flex items-center justify-center w-full">
+          <label
+            class="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300"
+          >
+            <div class="flex flex-col items-center justify-center pt-7">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                />
+              </svg>
+              <p
+                class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600"
+              >
+                Attach a file
+              </p>
             </div>
+            <input
+              type="file"
+              ref="input1"
+              class="opacity-0"
+              @change="previewImage"
+              accept="image/*"
+            />
+          </label>
         </div>
-      <div class="overflow-hidden max-h-5">
-        {{this.imageData.name}}
       </div>
-      <p><img id="output" style="height:200px"/></p>
+      <!-- <div class="overflow-hidden max-h-5">
+        {{ this.imageData.name }}
+        {{ this.imageURL }}
+      </div> -->
+      <div >
+        <img v-if="this.imageURL" :src="this.imageURL"  />
+        
+      </div>
     </div>
-</div> 
+  </div>
 
-
-    <button 
-            @click="submit_pet_post"
-            
-            type="submit"
-            id="submit_link"
-            class="py-4 bg-blue-400 w-full rounded text-blue-50 font-bold hover:bg-blue-700 my-2">
-            Submit
-          </button>
+  <button
+    @click="submit_pet_post"
+    type="submit"
+    id="submit_link"
+    class="py-4 bg-blue-400 w-full rounded text-blue-50 font-bold hover:bg-blue-700 my-2"
+  >
+    Submit
+  </button>
 </template>
 
 <script>
-import { getAuth }  from "firebase/auth";
-import { getDatabase, ref as dbRef,set,update,push,child,get } from "firebase/database";
-import { getStorage, ref as StoRef, uploadBytes,getDownloadURL} from 'firebase/storage';
+import { getAuth } from "firebase/auth";
+import {
+  getDatabase,
+  ref as dbRef,
+  set,
+  update,
+  push,
+  child,
+  get,
+} from "firebase/database";
+import {
+  getStorage,
+  ref as StoRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
 
-
-function writeData(userId,pname,pbreed,page,petphoto,animal_type) {
+function writeData(userId, pname, pbreed, page, petphoto, animal_type) {
   const db = getDatabase();
-  var newPetkey = push(child(dbRef(db), 'pets')).key;
+  var newPetkey = push(child(dbRef(db), "pets")).key;
   console.log(newPetkey);
-  const petid=newPetkey;
-    update(dbRef(db, 'pets/' + petid), {
+  const petid = newPetkey;
+  update(dbRef(db, "pets/" + petid), {
     petname: pname,
     petbreed: pbreed,
     petage: page,
     petphoto: petphoto,
-    animal_type:animal_type,
+    animal_type: animal_type,
   });
 
-  const userPets = dbRef(db, 'users/' + userId + "/petid_array");
+  const userPets = dbRef(db, "users/" + userId + "/petid_array");
   var pet_array = [];
-  get(child(dbRef(db), `users/${userId}/petid_array`)).then((snapshot) => {
-  if (snapshot.exists()) {
-    pet_array = snapshot.val();
-    console.log(pet_array);
-    pet_array.push(petid);
-    set(userPets,pet_array);
-    
-  } else {
-    pet_array.push(petid);
-    console.log("hI");
-    set(userPets,pet_array);
-    console.log("snapshot doesn't exist");
-  }
-}).catch((error) => {
-  console.error(error);
-});
+  get(child(dbRef(db), `users/${userId}/petid_array`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        pet_array = snapshot.val();
+        console.log(pet_array);
+        pet_array.push(petid);
+        set(userPets, pet_array);
+      } else {
+        pet_array.push(petid);
+        console.log("hI");
+        set(userPets, pet_array);
+        console.log("snapshot doesn't exist");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
   return petid;
 }
 
 
-
-
-
-
-
 export default {
-  data(){
-    return{
-      pname:"",
-      pbreed:"",
-      page:"",
-      imageData:"",
-      animal_type:"",
+  data() {
+    return {
+      pname: "",
+      pbreed: "",
+      page: "",
+      imageData: "",
+      animal_type: "",
       petid_array: [],
-      
-    }
+      imageURL: null,
+    };
   },
-  methods:{
+  methods: {
     submit_pet_post() {
         var usercreds = JSON.parse(localStorage.getItem("userCredential"));
         var userId= usercreds.uid
@@ -164,15 +199,19 @@ uploadBytes(storageRef,this.imageData)
           
       })
 },
+create () {
+      
+      const post = {
+        photo: this.img1,
+        caption: this.caption        
+      }
+  },
   
   
   
-},
-
+}
 }
 
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -20,7 +20,7 @@
 <template>
 
 <div class="container form-section mx-auto px-4 font-sans">
-  <form @submit.prevent="submit" class="shadow-md rounded-lg px-8 py-6 border-2 border-gray-200"> 
+  <form class="shadow-md rounded-lg px-8 py-6 border-2 border-gray-200"> 
     <h3 class="text-xl text-center font-bold text-yellow-500"> Services Near Me</h3>
       <div class="flex items-center justify-between space-x-4">
         <input id="autocomplete" type="text" placeholder="Location" v-model="address" class="mt-5 bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500"/>
@@ -31,7 +31,7 @@
           </svg>
         </button>
       </div>
-      <div class="container mt-5 text-red-400" v-show="error">{{error}}</div>
+ 
   </form>
 </div>
 
