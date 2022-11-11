43847e109c49a8e73b48747a4b794ae81df0f096
Cytan2000 <tanchiyong00@gmail.com>
2022-11-11 22:18:59 +0800

    homepage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -20,7 +20,7 @@
 <template>
 
 <div class="banner h-96 w-full form-section font-sans">
-  <form @submit.prevent="submit" class="shadow-md rounded-lg px-8 py-6 border-2 border-gray-200"> 
+  <form @submit.prevent="submit" class="content shadow-md rounded-lg px-8 py-6 "> 
     <h3 class="text-xl text-center font-bold text-yellow-500"> Services Near Me</h3>
       <div class="flex items-center justify-between space-x-4">
         <input id="autocomplete" type="text" placeholder="Location" v-model="address" class="mt-5 bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500"/>
